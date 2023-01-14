595dd7069880502755bd461e8c8c413b4179604e
Vaibhav Bhandari <vaibhavb@gmail.com>
2023-01-14 22:59:47 +0000

    Updates

--- web/app.py
+++ web/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-import os, yaml
+import os
+import yaml
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    flashcards = {}
-    with open('../flashcards/quotes.yml') as card:
-        card = yaml.load(card)
-        flashcards = card
+    flashcards = list()
+    with open('../flashcards/Basic_ascending_sort.yml', 'r') as card:
+        card = yaml.safe_load(card)
+        flashcards.append(card)
     return render_template("index.html", flashcards=flashcards)
